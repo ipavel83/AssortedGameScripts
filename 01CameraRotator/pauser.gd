594fbe8e9dfbebb02a0db5bf7041faa76097f8ef
atomknack <110350925+atomknack@users.noreply.github.com>
2024-03-19 14:44:09 +0400

    other type of pause

--- 01CameraRotator/pauser.gd
+++ 01CameraRotator/pauser.gd
@@ -5,6 +5,10 @@ extends Node3D
 func _ready():
 	pass # Replace with function body.
 
+#func _unhandled_key_input(event):
+#	if event.is_action_pressed("key_pause"):
+#		var new_pause_state = not get_tree().paused
+#		get_tree().paused = new_pause_state
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
